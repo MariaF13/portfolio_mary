47c4d17a764c93606dac80fa62e56cf60b31f3de
Maria Mendonça <mf414676@gmail.com>
2024-08-10 18:23:38 -0400

    Correcting footer

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -88,7 +88,6 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="Background Shape" />
     </section>
   );
 };
